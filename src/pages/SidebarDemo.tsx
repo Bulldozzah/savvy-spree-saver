07bfbc21cd034459a67ca4f02f93f38dac93bb61
Bulldozzah <Chilunguabel@yahoo.com>
2026-04-15 12:07:24 +0200

    Update 1

--- src/pages/SidebarDemo.tsx
+++ src/pages/SidebarDemo.tsx
@@ -112,7 +112,7 @@ const Logo = () => {
         animate={{ opacity: 1 }}
         className="font-medium text-foreground whitespace-pre"
       >
-        Savio
+        Savio-shop
       </motion.span>
     </Link>
   );
